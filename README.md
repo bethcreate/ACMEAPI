cd5285bcdcf8f6c6ebcfe0fc5f389a9cf02c127d
bethcreate <kamaubetth@gmail.com>
2023-04-12 14:43:55 +0300

    user registration

---
+++ README.md
@@ -0,0 +1,37 @@
+##
+
+ACME Api
+
+##
+
+This is a restfull api that enables ACME team to Create, read, update and delete product.
+It is built using python Flask framework.
+
+##
+
+app.py : the main file in the flask app
+templates : contains all the frontend files
+database : contains the database schema
+reqirements.txt : contains all the libraries and packages used in the project
+
+##
+
+
+##
+
+Installation
+
+##
+
+$ pip install -r requirements.txt
+
+##
+
+Running the app
+
+##
+
+$ flask run
+
+
+
--- app.py
+++ app.py
@@ -1,22 +1,118 @@
 from random import Random
 import sqlite3
-from flask import Flask, jsonify, render_template, request, redirect
+from flask_sqlalchemy import SQLAlchemy
+from flask import Flask, jsonify, render_template, request, redirect, session, url_for
 from models import Product
+from flask_login import UserMixin, login_user, LoginManager, login_required, logout_user, current_user
+from wtforms import StringField, PasswordField, SubmitField
+from wtforms.validators import InputRequired, Length, ValidationError
+from flask_wtf import FlaskForm
+from flask_bcrypt import Bcrypt
+
 
 app = Flask(__name__)
-db = "mydatabase.db"
+db = SQLAlchemy(app)
+db.init_app(app)
+bcrpt=Bcrypt(app)
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///database.db"
+app.config['SECRET_KEY'] = 'scretekey'
+
+# db = "mydatabase.db"
+
+login_manager = LoginManager()
+login_manager.init_app(app)
+login_manager.login_view="login"
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
+
+
+
+class User(db.Model, UserMixin):
+    id=db.Column(db.Integer,primary_key=True)
+    username = db.Column(db.String(20), nullable=False, unique=True)
+    password = db.Column(db.String(80), nullable=True) 
+
+class RegistrationForm(FlaskForm):
+    username = StringField(validators=[InputRequired(), Length(min=4,max=20)],render_kw={"placeholder":"username"})
+    
+    password = PasswordField(validators=[InputRequired(), Length(min=4,max=20)],render_kw={"placeholder": "password"})
+    
+    submit = SubmitField("Register")
+
+    def validate(self,username):
+        existing_user_username= User.query.filter_by(
+            username=username.data).first()
+        if existing_user_username:
+            raise ValidationError("The username already exists")
+    
+
+class LoginForm(FlaskForm):
+    username = StringField(validators=[InputRequired(), Length(min=4,max=20)], render_kw={"placeholder":"username"})
+    
+    password = PasswordField(validators=[InputRequired(), Length(min=4,max=20)],render_kw={"placeholder": "password"})
+    
+    submit = SubmitField("login")
+ 
+
+# Home page
+@app.route('/')
+def home():
+    products = Product.read_all(db)
+    return render_template('home.html',{products})
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        user=User.query.filter.filter_by(username=form.username.data).first()
+        if user:
+            if bcrpt.check_password_hash(user.password, form.password.data):
+                login_user(user)
+                return redirect(url_for('dashboard'))
+
+    return render_template('login.html', form=form)
+
+@app.route('/dashboard', methods=['GET', 'POST'])
+@login_required
+def dashboard():
+    return render_template('dashboard.html')
+
+@app.route('/logout', methods=['GET', 'POST'])
+@login_required
+def logout():
+    logout_user()
+    return redirect(url_for('login'))
+
+
+
+@app.route('/register' , methods=['GET', 'POST'])
+def register():
+    form = RegistrationForm
+
+    if form.validate_on_submit():
+        hashed_password = bcrpt.generate_password_hash(form.password.data)
+        new_user = User(username= form.username.data, password=hashed_password)
+        db.session.add(new_user)
+        db.session.commit()
+        return redirect(url_for('Login'))
+
+    return render_template('register.html', form=form)
+
+    # if 'username' in session:
+    #     return render_template('home.html', username=session['username'])
+    # else:
+    #     return redirect('/login')
+
+
 
 
 def get_db_connection():
     conn = sqlite3.connect(db)
     conn.row_factory = sqlite3.Row
     return conn
-
-
-@app.route("/")
-def home():
-    products = Product.read_all(db)
-    return jsonify(products)
 
 
 @app.route("/add", methods=["GET", "POST"])
@@ -58,3 +154,7 @@ def delete(id):
     conn.commit()
     conn.close()
     return redirect("/")
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
